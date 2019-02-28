89ac2b77b803cfdb9868680f3ea5883e0df75029
Jaxon Porter <jaxonutah@gmail.com>
2019-02-28 13:52:34 -0700

    added user, board, list, and task models with controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users
+
+  root "boards#index"
+
+  resources :boards do
+    resources :lists
+  end
+
+  resources :lists do
+    resources :tasks
+  end
+  
 end
